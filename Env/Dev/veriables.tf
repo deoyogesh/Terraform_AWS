83e380b2cac77f74b317f8ebd0c91bea9a53f9bc
deoyogesh <deoyogesh1703@gmail.com>
2025-11-02 00:18:20 +0530

    fixing variables.tf

--- Env/Dev/veriables.tf
+++ Env/Dev/veriables.tf
@@ -1,8 +1,10 @@
 variable "name"           { default = "dev" }
 variable "aws_region"     { default = "ap-south-1" }
 variable "vpc_cidr"       { default = "10.0.0.0/16" }
-variable "azs"            { default = ["ap-south-1a"] }
-variable "public_cidrs"   { default = ["10.0.1.0/24"] }
+variable "azs"            { default = ["ap-south-1a","ap-south-1b"] }   # 2 AZs if 2 subnets
+variable "public_subnets_cidrs"  { default = ["10.0.1.0/24","10.0.2.0/24"] }
+variable "private_subnets_cidrs" { default = ["10.0.101.0/24","10.0.102.0/24"] }  # <-- add
+
 variable "admin_cidr"     { default = "0.0.0.0/0" }
 variable "ec2_ami"        { default = "ami-0f58b397bc5c1f2e8" }
 variable "instance_type"  { default = "t3.micro" }
