994d6e669dcf6f749fc1d8d7d0384950b1aa506a
deoyogesh <deoyogesh1703@gmail.com>
2025-11-02 02:01:50 +0530

    Add required providers to provider.tf

--- Env/Dev/provider.tf
+++ Env/Dev/provider.tf
@@ -1,5 +1,11 @@
+terraform {
+  required_providers {
+    aws   = { source = "hashicorp/aws",   version = "~> 5.0" }
+    tls   = { source = "hashicorp/tls",   version = "~> 4.0" }
+    local = { source = "hashicorp/local", version = "~> 2.4" }
+  }
+}
 provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-}
